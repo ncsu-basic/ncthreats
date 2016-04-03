21d4ec498288a77e06ca7010b62e9a5eb3f00692
Jim White <jbwhite2@ncsu.edu>
2016-04-03 17:10:02 -0400

    sql for wms

---
+++ docs/wms_layers.sql
@@ -0,0 +1,11 @@
+-- Urban Growth
+
+SELECT urban.urb10dt as dt, huc12nc.*
+FROM urban, huc12nc
+WHERE urban.huc_12 = huc12nc.huc_12
+
+-- habitat loss forest
+
+select lcscen_x.frst10dt as dt, huc12nc.*
+from lcscen_x, huc12nc
+WHERE lcscen_x.huc_12 = huc12nc.huc_12
