4c29e41572ffde516ec064e3978df0916dfd9990
jim white <jbwhite2@ncsu.edu>
2016-12-02 10:56:50 -0500

    mountains

--- pages/mountains.php
+++ pages/mountains.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<p>mountains in php</p>";
+// echo "<p>mountains in php</p>";
 $dbname = "ncthreats";
 $host = "localhost";
 $dbuser = "postgres";
@@ -13,10 +13,25 @@ $stmt = $dbh->prepare($query);
 $stmt->execute();
 $result = $stmt->fetchAll();
 
+echo "<h4>Aquatic</h4>";
+echo "<table>";
+
 foreach ($result as $key => $value) {
     // print $key;
 
     if ($value['region'] == 'Coastal Plain' && $value['type'] == 'Aquatic') {
+        // print_r($value);
+        // print "<br>";
+        // print "<br>";
+        echo "<tr><td>{$value['communityname']}</td></tr>";
+    }
+}
+echo "</table>";
+echo "<h4>Terrestrial</h4>";
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Coastal Plain' && $value['type'] == 'Terrestrial') {
         print_r($value);
         print "<br>";
         print "<br>";
