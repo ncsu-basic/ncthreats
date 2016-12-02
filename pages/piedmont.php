ccecd40a86a2f6cb6c97a47ac5953a117b2166f5
jim white <jbwhite2@ncsu.edu>
2016-12-02 11:22:29 -0500

    pages

--- pages/piedmont.php
+++ pages/piedmont.php
@@ -1,1 +1,73 @@
-    <p>piedmont</p>
+<?php
+
+// echo "<p>mountains in php</p>";
+$dbname = "ncthreats";
+$host = "localhost";
+$dbuser = "postgres";
+
+$dbh = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser);
+
+$query = "select * from coa_keylist";
+
+$stmt = $dbh->prepare($query);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+echo "<h4>Aquatic</h4>";
+echo "<table>";
+
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Piedmont' && $value['type'] == 'Aquatic') {
+        // print_r($value);
+        // print "<br>";
+        // print "<br>";
+        echo "<tr>";
+        echo "<td class='coatables'>{$value['communityname']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_all']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_sgcn']}</td>";
+
+        echo "</tr>";
+    }
+}
+echo "</table>";
+echo "<h4>Terrestrial</h4>";
+echo "<table>";
+
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Piedmont' && $value['type'] == 'Terrestrial') {
+        echo "<tr>";
+        echo "<td class='coatables'>{$value['communityname']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_all']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_sgcn']}</td>";
+
+        echo "</tr>";
+    }
+}
+echo "</table>";
+
+echo "<h4>Wetland</h4>";
+echo "<table>";
+
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Piedmont' && $value['type'] == 'Wetland') {
+        echo "<tr>";
+        echo "<td class='coatables'>{$value['communityname']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_all']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_sgcn']}</td>";
+
+        echo "</tr>";
+    }
+}
+echo "</table>";
--- pages/sandhills.php
+++ pages/sandhills.php
@@ -1,1 +1,73 @@
-    <p>coastal</p>
+<?php
+
+// echo "<p>mountains in php</p>";
+$dbname = "ncthreats";
+$host = "localhost";
+$dbuser = "postgres";
+
+$dbh = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser);
+
+$query = "select * from coa_keylist";
+
+$stmt = $dbh->prepare($query);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+echo "<h4>Aquatic</h4>";
+echo "<table>";
+
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Sandhills' && $value['type'] == 'Aquatic') {
+        // print_r($value);
+        // print "<br>";
+        // print "<br>";
+        echo "<tr>";
+        echo "<td class='coatables'>{$value['communityname']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_all']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_sgcn']}</td>";
+
+        echo "</tr>";
+    }
+}
+echo "</table>";
+echo "<h4>Terrestrial</h4>";
+echo "<table>";
+
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Sandhills' && $value['type'] == 'Terrestrial') {
+        echo "<tr>";
+        echo "<td class='coatables'>{$value['communityname']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_all']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_sgcn']}</td>";
+
+        echo "</tr>";
+    }
+}
+echo "</table>";
+
+echo "<h4>Wetland</h4>";
+echo "<table>";
+
+foreach ($result as $key => $value) {
+    // print $key;
+
+    if ($value['region'] == 'Sandhills' && $value['type'] == 'Wetland') {
+        echo "<tr>";
+        echo "<td class='coatables'>{$value['communityname']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_all']}</td>";
+        echo "<td class='coatables'><input name='reg_com' type='radio' /></td>";
+        echo "<td class='coatables'>{$value['nsppwrc_sgcn']}</td>";
+
+        echo "</tr>";
+    }
+}
+echo "</table>";
