5753aad2f476c0174a3b852034e106fa7a591fd6
Jim White <jbwhite2@ncsu.edu>
2017-03-30 16:23:16 -0400

    sql

--- docs/coa/basin_lbls.sql
+++ docs/coa/basin_lbls.sql
@@ -1,20 +1,20 @@
 
 delete from ncwrc_basin_lbl;
 
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Broad', st_GeomFromText('POINT(-81.9 35.4)'));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Cape Fear', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Catawba', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Chowan', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('French Broad', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Hiwassee', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Little Tennessee', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Lumber', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Neuse', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('New', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Pasquotank', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Roanoke', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Savannah', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Tar - Pamlico', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Watauga', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('White Oak', st_GeomFromText('POINT()', 4326));
-INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Yadkin - PeeDee', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Broad', st_GeomFromText('POINT(-81.9 35.4)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Cape Fear', st_GeomFromText('POINT(-79 35.11)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Catawba', st_GeomFromText('POINT(-81.3 35.7)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Chowan', st_GeomFromText('POINT(-76.98 36.4)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('French Broad', st_GeomFromText('POINT(-82.65 35.6)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Hiwassee', st_GeomFromText('POINT(-84 35.1)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Little Tennessee', st_GeomFromText('POINT(-83.5 35.37)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Lumber', st_GeomFromText('POINT(-78.9 34.55)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Neuse', st_GeomFromText('POINT(-78 35.46)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('New', st_GeomFromText('POINT(-81.46 36.42)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Pasquotank', st_GeomFromText('POINT(-76.21 35.97)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Roanoke', st_GeomFromText('POINT(-79.47 36.43)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Savannah', st_GeomFromText('POINT(-83 35.07)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Tar - Pamlico', st_GeomFromText('POINT(-77.43 35.79)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Watauga', st_GeomFromText('POINT(-81.87 36.25)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('White Oak', st_GeomFromText('POINT(-77.19 34.7)', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Yadkin - PeeDee', st_GeomFromText('POINT(-80.5 35.5)', 4326));
