918a7c9ff72bee4b4d3aec6e2fe321c0e3f4f0e6
Jim White <jbwhite2@ncsu.edu>
2017-03-30 16:12:09 -0400

    sql

---
+++ docs/coa/basin_lbls.sql
@@ -0,0 +1,16 @@
+
+delete from ncwrc_basin_lbl;
+
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Broad', st_GeomFromText('POINT(-81.9 35.4)'));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Cape Fear', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Catawba', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Chowan', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('French Broad', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('Hiwassee', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
+INSERT INTO ncwrc_basin_lbl (label, the_geom) VALUES ('', st_GeomFromText('POINT()', 4326));
