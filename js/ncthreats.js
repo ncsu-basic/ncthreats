2cd4f9c52362f507d4eaa03f69c9108422914bc8
Jim White <jbwhite2@ncsu.edu>
2018-02-02 11:00:40 -0500

    docs for edit server name

--- js/ncthreats.js
+++ js/ncthreats.js
@@ -3,9 +3,11 @@ var map;
 Ext.onReady(function() {
     "use strict";
 
+    // change hostname and server uri for server or localhost
 
-    var HOST_NAME = "http://tecumseh.zo.ncsu.edu/coa/";
-    var SERVER_URI = "http://tecumseh.zo.ncsu.edu/";
+
+    var HOST_NAME = "http://localhost/coa/";
+    var SERVER_URI = "http://localhost/";
 
     var resource = SERVER_URI + "wps/0";
     var batch_aoi = false;
