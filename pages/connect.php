31aa69d786f57063db6dc55a7d3f30232672bb1a
Vaclav Petras <wenzeslaus@gmail.com>
2018-03-01 16:01:12 -0500

    restore the common db connection from 55c92e3

--- pages/connect.php
+++ pages/connect.php
@@ -1,6 +1,6 @@
 <?php
 
 // change to the public ip to get conn to local db server
-$connect_string = "pgsql:dbname=postgres user=postgres password=mysecretpassword host=db"
+$connect_string = "pgsql:dbname=ncthreats user=postgres host=localhost"
 
 ?>
